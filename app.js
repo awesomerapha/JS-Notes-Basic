fa3da06caa4d7080c25529ba095e9c883eec97e0
awesomerapha <raphael.morgenshtern@gmail.com>
2019-09-09 11:38:48 -0400

    9/9/19

---
+++ app.js
@@ -0,0 +1,21 @@
+///////////Primitives
+/* let numbers = 4;
+numbers = 5;
+let numbersArrays = [1,2,3,4,5];
+console.log(numbers);
+
+let string = 'string';
+let strings = "strings";
+
+let boolean = true; //also false */
+
+//////////
+
+const firstName = 'Rob';
+const job = 'Student';
+const birthYear = 2002;
+let isMarried = false;
+
+console.log("Hello my name is " + firstName + "I am a" + job + "and I was born in " + birthYear + " and am I married?")
+
+console.log(`Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear} and am I married?` );
