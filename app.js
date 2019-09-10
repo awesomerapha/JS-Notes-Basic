518a1b6e3f19f4071312e0b103bb3779e755890e
awesomerapha <raphael.morgenshtern@gmail.com>
2019-09-10 11:43:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,30 @@ let boolean = true; //also false */
 
 //////////
 
-const firstName = 'Rob';
-const job = 'Student';
-const birthYear = 2002;
-let isMarried = false;
+//const firstName = 'Rob';
+//const job = 'Student';
+//const birthYear = 2002;
+//let isMarried = false;
 
-console.log("Hello my name is " + firstName + "I am a" + job + "and I was born in " + birthYear + " and am I married?");
+//console.log("Hello my name is " + firstName + "I am a" + job + "and I was born in " + birthYear + " and am I married?");
 
-console.log(`Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear} and am I married?` );
+//console.log(`Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear} and am I married?` );
+
+
+const now = new Date().getFullYear();
+const yearIlsa = 2003;
+let fullAge= 18;
+let isFullAge =now - yearIlsa >= fullAge;
+//console.log(isFullAge);
+let ageWill = now - 2002;
+let ageNick = 18;
+let averageAge = (ageNick + ageWill) / 2;
+console.log(averageAge);
+
+let x,y,q,z; 
+/*x = 5;
+y = x;
+z = y;
+q = z;
+console.log(z); 
+*/
