8b3bdd75270ac0d22199546dc60d4d6fe25ce771
awesomerapha <raphael.morgenshtern@gmail.com>
2019-10-08 11:11:48 -0400

    ik

---
+++ script.js
@@ -0,0 +1,42 @@
+class Person{
+    construction(age,name){
+        this.age = age;
+        this.name = name;
+    }
+    calculateBirthYear(){
+        return 2019 - this.age;
+    }
+}
+const steve = new Person(44, 'Steve');
+console.log(steve.calculateBirthYear());
+
+
+//one class to create the object person, album, movie ****Model
+//take arguments
+
+
+
+// *****View // class for the UI which contains methods
+
+//creates/inserts the html for the UI DISPLAY
+
+//clear fields method
+
+//remove something/object
+
+
+
+
+//Controller combines UI and model
+//event handler with function
+//get values
+
+
+//instantiate new class/object album with variable name
+
+//instantiate new UI const ui = new UI()
+
+//call relevant methods
+
+
+//seperate event handler for removing/deleting
